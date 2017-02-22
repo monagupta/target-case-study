23023d4f68a5313284649a9573b934ed3147a909
Mona Gupta <monagupta43@gmail.com>
2017-02-22 00:03:41 -0600

    Add dummy seed data to mongoDb prices collection

--- src/main/java/com/example/ProductsApplication.java
+++ src/main/java/com/example/ProductsApplication.java
@@ -3,10 +3,67 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.UnknownHostException;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ProductsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApplication.class, args);
+        addSeedData();
+    }
+
+    /**
+        Add seed data to prices database, if and only if the
+        prices database is empty.
+    **/
+    private static void addSeedData() {
+
+        MongoClientURI uri  = new MongoClientURI(System.getenv("MONGODB_URI"));
+        MongoClient client = new MongoClient(uri);
+        MongoDatabase db = client.getDatabase(uri.getDatabase());
+
+        MongoCollection<Document> prices = db.getCollection("prices");
+
+        // Return early if the database is non-empty
+        if (prices.count() != 0) return;
+
+        List<Document> seedData = new ArrayList<Document>();
+
+        seedData.add(new Document("id", 1)
+            .append("value", 1.11)
+            .append("currency_code", "USD_1")
+        );
+
+        seedData.add(new Document("id", 2)
+            .append("value", 2.22)
+            .append("currency_code", "USD_2")
+        );
+
+        seedData.add(new Document("id", 3)
+            .append("value", 3.33)
+            .append("currency_code", "USD_3")
+        );
+
+        prices.insertMany(seedData);
 	}
 }
